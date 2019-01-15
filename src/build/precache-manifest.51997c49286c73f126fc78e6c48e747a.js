86e750b5edb983ef720148f7975164d3975099df
Ken yip <ken20206@gmail.com>
2019-01-16 00:18:35 +0800

    add port

---
+++ src/build/precache-manifest.51997c49286c73f126fc78e6c48e747a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "146017fa5841b2d477ee3ca90810a3d3",
+    "url": "/static/media/loginBackground.146017fa.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "45ddb1571a8625da089a",
+    "url": "/static/js/main.45ddb157.chunk.js"
+  },
+  {
+    "revision": "bb55bba4a516c18061d8",
+    "url": "/static/js/1.bb55bba4.chunk.js"
+  },
+  {
+    "revision": "45ddb1571a8625da089a",
+    "url": "/static/css/main.808ada79.chunk.css"
+  },
+  {
+    "revision": "bb55bba4a516c18061d8",
+    "url": "/static/css/1.494e3b1f.chunk.css"
+  },
+  {
+    "revision": "809f37dc78ab259e3a3cd2f9ff22f737",
+    "url": "/index.html"
+  }
+];
